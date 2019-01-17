00837b60ed950e934e11a21f0737d32652336945
infsolution <clsinfsolution@gmail.com>
2019-01-16 22:55:37 -0300

    alter google for busca

--- makevisio/urls.py
+++ makevisio/urls.py
@@ -1,6 +1,7 @@
+ # -*- coding: utf-8 -*-
 from django.urls import path, include
 from makevisio import views
 urlpatterns = [
     path('',views.index, name='index'),
-    path('search',views.search_google, name='google'),
+    path('search',views.search_google, name='busca'),
 ]
