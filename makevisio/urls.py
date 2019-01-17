b77d2f8a164d14ad38a20acd877814d2ec7bc81f
infsolution <clsinfsolution@gmail.com>
2019-01-16 23:00:54 -0300

    change buscar

--- makevisio/urls.py
+++ makevisio/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from makevisio import views
 urlpatterns = [
     path('',views.index, name='index'),
-    path('search',views.search_google, name='buscar'),
+    path('buscar',views.search_google, name='buscar_item'),
 ]
