54934b350e7bbf4ca3ba64a7739f9cdbfc30d464
infsolution <clsinfsolution@gmail.com>
2019-01-31 01:19:33 +0000

    urls

--- makevisio/urls.py
+++ makevisio/urls.py
@@ -2,9 +2,9 @@
 from django.urls import path
 from makevisio import views
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('buscar',views.search_google, name='buscar_item'),
-    path('api/iped/padaria/',views.PadariaList.as_view(), name=views.PadariaList.name),
+	path('',views.index, name='index'),
+	path('buscar',views.search_google, name='buscar_item'),
+	path('api/iped/padaria/',views.PadariaList.as_view(), name=views.PadariaList.name),
 	path('api/iped/padaria/<int:pk>',views.PadariaDetail.as_view(), name=views.PadariaDetail.name),
 	path('api/iped/users/',views.UserList.as_view(),name=views.UserList.name),
 	path('api/iped/users/<int:pk>/',views.UserDetail.as_view(),name=views.UserDetail.name),
