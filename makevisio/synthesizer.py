8e57a53b5646156d631929ba3ba76bd84f33d60c
infsolution <clsinfsolution@gmail.com>
2019-10-04 16:57:41 -0300

    add sudo in call espeak

--- makevisio/synthesizer.py
+++ makevisio/synthesizer.py
@@ -3,16 +3,16 @@ import os
 class Synthesizer:			
 
 	def synthesizer(self, arg):
-		inicio = "espeak -vpt -k 20 '"
+		inicio = "sudo espeak -vpt -k 20 '"
 		comand = "' -w "
-		#path = "/var/www/html/visio/media/"
-		path = "/home/cicero/Documentos/IFPI/TCC/visio/media/"
+		path = "/var/www/html/visio/media/"
+		#path = "/home/cicero/Documentos/IFPI/TCC/visio/media/"
 		name = self.replace_all(arg[0:15])
 		print(name)
 		exte =".wav"
 		#os.system(inicio+arg+comand+path+name+exte)
 		os.system(u' '.join((inicio,arg,comand,path,name,exte)).encode('utf-8'))
-		print(u' '.join((inicio,arg,comand,path,name,exte)).encode('utf-8'))
+		#print(u' '.join((inicio,arg,comand,path,name,exte)).encode('utf-8'))
 		return "media/"+name+exte
 		
 
